1f078cc2da453ff029562be6005fc863a4a4f3d8
Felps <74378136+Felpxz-developer@users.noreply.github.com>
2022-09-08 20:06:55 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,21 @@
-function logar(){var login = document.getElementById('login').value;var senha = document.getElementById('senha').value;if (login == 'felipe' && senha == '123') {window.location.href='/spec/';} else if (login == 'allyson' && senha == '4469delas') {window.location.href='/spec/';} else if (login == 'juca' && senha == 'jj') {window.location.href='/spec/';}else if(login == 'admin' && senha == 'admin'){window.location.href='/spec/';}else if ((login == ""  && senha == "") || (login == "") || (senha == "")){alert('Ta faltando Alguma coisa ai men !')}else{alert ('Coe men ??'),window.location.href='https://xvideos.com/?k=gay';}}
+function logar(){
+
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if (login == 'felipe' && senha == '123') {window.location.href='/spec/';} 
+
+    else if (login == 'allyson' && senha == '4469delas') {window.location.href='/spec/';} 
+
+    else if (login == 'juca' && senha == 'jj') {window.location.href='/spec/';}
+
+    else if(login == 'daga17' && senha == 'loja10506'){window.location.href='/spec/';}
+
+    else if(login == 'admin' && senha == 'admin'){window.location.href='/spec/';}
+
+    else if ((login == ""  && senha == "") || (login == "") || (senha == ""))
+
+    {alert('Ta faltando alguma coisa ai man !')}
+
+    else{alert ('Coe man ??'),window.location.href='https://xvideos.com/?k=gay';}}
 // Coe men ta na malicia né ? 
